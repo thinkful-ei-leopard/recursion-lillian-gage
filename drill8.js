b61982214c34d01da67213000142561be4192056
Lillian Burnside <lillian.burnside@gmail.com>
2020-03-30 16:40:22 -0500

    finished drill 8

--- drill8.js
+++ drill8.js
@@ -1,23 +1,35 @@
 'use strict';
 
-function mazeSolver(maze, n = 0, m = 0) {
-  console.log(n, m);
-  console.log(maze.length);
-  if(maze[n][m] === 'e') {
+function mazeSolver(maze, row = 0, col = 0) {
+
+  if(maze[row][col] === 'e') {
     return '';
   }
-  if(n < maze[n].length - 1 && (maze[n + 1][m] !== '*' && maze[n + 1][m])) {
-    return 'R' + mazeSolver(maze, n + 1, m);
+  if(col < maze[row].length - 1 && (maze[row][col + 1] !== '*' && maze[row][col + 1])) {
+    maze[row][col] = '*';
+    const next = 'R' + mazeSolver(maze, row, col + 1);
+    maze[row][col] = ' ';
+    return next ;
   }
-  // if(maze[n - 1][m] === ' ') {
-  //   return 'L' + mazeSolver(maze, n - 1 , m);
-  // }
-  if(m < maze.length - 1 && (maze[n][m + 1] !== '*' && maze[n][m + 1])) {
-    return 'D' + mazeSolver(maze, n, m + 1);
+  if(row < maze.length - 1 && (maze[row + 1][col] !== '*' && maze[row + 1][col])) {
+    maze[row][col] = '*';
+    const next = 'D' + mazeSolver(maze, row + 1, col);
+    maze[row][col] = ' ';
+    return next;
   }
-  // if(maze[n][m - 1] === ' ') {
-  //   return 'U' + mazeSolver(maze, n, m - 1);
-  // }
+  if(col > 0 && (maze[row][col - 1] !== '*' && maze[row][col - 1])) {
+    maze[row][col] = '*';
+    const next = 'L' + mazeSolver(maze, row, col - 1);
+    maze[row][col] = ' ';
+    return next;
+  }
+  if(row > 0 && (maze[row - 1][col] !== '*' && maze[row - 1][col])) {
+    maze[row][col] = '*';
+    const next = 'U' + mazeSolver(maze, row - 1 , col);
+    maze[row][col] = ' ';
+    return next;
+  }
+
 }
 
 
@@ -36,6 +48,17 @@ let maze = [
   [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
 
+let medMaze = [
+  [' ', ' ', ' ', '*', ' '],
+  ['*', '*', ' ', '*', ' '],
+  [' ', ' ', ' ', ' ', ' '],
+  [' ', '*', '*', '*', '*'],
+  [' ', ' ', ' ', ' ', 'e']
+];
+
+//console.log(mazeSolver(mySmallMaze));
+//console.log(mazeSolver(medMaze));
 console.log(mazeSolver(maze));
 
+//console.log(maze[1][0]); = *
 //console.log(maze[0][3]); = *
