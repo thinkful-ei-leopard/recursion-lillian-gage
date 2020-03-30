b059b594ac759ae69b46622c3d62da303c2e66ef
Lillian Burnside <lillian.burnside@gmail.com>
2020-03-30 11:47:13 -0500

    drill 5 start

---
+++ drill5.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function stringSplitter(str, separator='/') {
+  if(char === '/'){
+    return arr= ...arr + 
+  }
+  if(){
+    
+  }
+
+  const char = str[0]
+  return char + stringSplitter(str.slice(1))
+}
+
+stringSplitter('02/20/2020')
