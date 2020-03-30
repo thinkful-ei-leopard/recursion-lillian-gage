39d3ac2efc9d76579e5965375c340a80c95ac7b1
gage117 <57643501+gage117@users.noreply.github.com>
2020-03-30 14:20:34 -0500

    Added drill6.js

---
+++ drill6.js
@@ -0,0 +1,13 @@
+function fibonacci(n, fibSeq = []) {
+  if(fibSeq.length === n) {
+    return fibSeq;
+  }
+  if (fibSeq.length < 2) {
+    fibSeq.push(1);
+    return fibonacci(n, fibSeq);
+  }
+  fibSeq.push(fibSeq[fibSeq.length -1] + fibSeq[fibSeq.length -2]);
+  return fibonacci(n, fibSeq);
+}
+
+console.log(fibonacci(7));
